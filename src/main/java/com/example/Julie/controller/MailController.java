3843b1d812022c6cbfb08c4269b457ab70efee60
bryan-atencia <bryan.atencia@thetiebar.com>
2020-11-10 09:28:21 -0600

    add env variables

--- src/main/java/com/example/Julie/controller/MailController.java
+++ src/main/java/com/example/Julie/controller/MailController.java
@@ -11,24 +11,30 @@ import javax.mail.*;
 import javax.mail.internet.*;
 
 import com.example.Julie.model.Mail;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class MailController {
 
+	@Autowired
+	private Environment env;
+
 	@PostMapping("/sendMessage")
 	@ResponseBody
-	public static void sendMessage(@RequestParam("contact_name") String contact_name,
-																 @RequestParam("contact_email") String contact_email,
-																 @RequestParam("contact_phone") String contact_phone,
-															   @RequestParam("contact_message") String contact_message,
-														 	   @RequestParam("contact_other") String contact_other) {
+	public void sendMessage(@RequestParam("contact_name") String contact_name,
+											    @RequestParam("contact_email") String contact_email,
+												  @RequestParam("contact_phone") String contact_phone,
+											    @RequestParam("contact_message") String contact_message,
+										 	    @RequestParam("contact_other") String contact_other) {
 	   //from,password,to,subject,message
 		// System.out.println(contact_name + contact_email + contact_phone + contact_message + contact_other);
-		Mail.send("bryanatencia224@gmail.com",
-							  "Julie1@34",
-							  "bryan.atencia@thetiebar.com",
-							  "Message from " + contact_name,
-							  "Email: " + contact_email + " Phone Number: " + contact_phone + " Message: " + contact_message + " PS: " + contact_other);
-	  //change from, password and to
+
+		 Mail.send(env.getProperty("EMAIL"),
+	 						 env.getProperty("EMAIL_PASSWORD"),
+	 						 env.getProperty("EMAIL_TO"),
+	 						 "Message from " + contact_name,
+	 						 "Email: " + contact_email + " Phone Number: " + contact_phone + " Message: " + contact_message + " PS: " + contact_other);
+
 	 }
 }
